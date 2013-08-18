7ccc90eb7ea7a443e5f474df245e3170b97dbdae
ankit1987 <ankittyagi.mnnit@gmail.com>
2013-08-19 00:13:00 +0530

    commiting threadpool for unit testing to hit the webservice and updated readme.txt

--- AutoSuggest/src/unitTesting/CallService.java
+++ AutoSuggest/src/unitTesting/CallService.java
@@ -1,16 +1,64 @@
 package unitTesting;
 
-public class CallService implements Runnable {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CallService  {
 	
 	
-	public CallService(){
+	public static void main(String args[]) {
+		
+	    ExecutorService service = Executors.newFixedThreadPool(100);
+	    for (int i =0; i<1000; i++){
+	        service.submit(new Task());
+	    }
+	  
 		
 	}
 
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		
-	}
+}
 
+final class Task implements Runnable{
+ String [] str=new String[]{"how to ","how t","how to french","how to write a cover letter","create index","term frequency","inverse document frequency","document frequency"};
+
+
+ @Override
+ public void run() {
+ 
+	 Random rng=new Random();
+	 int i=rng.nextInt(str.length);
+	 String query=str[i];
+	 /*String url_name="http://ec2-50-112-143-235.us-west-2.compute.amazonaws.com:8080/s?q="+query;
+	 System.out.println( url_name);*/
+	 
+	 //long startTime=System.currentTimeMillis();
+		System.out.println();
+		URL url;
+		try {
+			long startTime=System.currentTimeMillis();
+			url = new URL("http://ec2-50-112-143-235.us-west-2.compute.amazonaws.com:8080/s?q=bu");
+			BufferedReader in;
+			in = new BufferedReader(new InputStreamReader(url.openStream()));
+			long endTime=System.currentTimeMillis();
+			System.out.println("time spend in query" + (endTime-startTime));
+			String line = in.readLine(); 
+
+			System.out.println( line );	
+			/*System.out.println("time spend in query" + (endTime-startTime));*/
+
+		}catch (MalformedURLException e) {
+		
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
+	 
+ }
 }
--- ReadMe.txt
+++ ReadMe.txt
@@ -1,23 +1,29 @@
 This projet is devloped to make Google autocomplete functionality. 
 
 I have used following opensource api
-	1-Lucene (to create the in memory map for queries)
+	1-Lucene (to create the in memory map for queries according to weight)
 	2-Serlvet(to get the json response)
 	3-Gson (to change httpservlet response into json object)
 
 query data file is needed to be placed WebContent\WEB-INF\DataFiles. i am expecting file in the following format
 
-		<query, weight>
-example how to boil an egg,4
+	<query, weight>
+ex. how to boil an egg,4
 
 
 currently I am returning top 4 queries according to weight. this is configuration parameter(MAX_RETURN_QUERIES) in web.xml.	
 	
 	
 ===Creating war to deploy in webapps folder of tomcat===
-use ant war, this will create war file in dist folder.
+use ant war, this will create war file in dist folder. you can deploy the generated war(AutoSuggest.war) to local webapps folder for localtesting.
+if you running tomcat locally then use the following link to test http://localhost:8080/AutoSuggest/s?q=[place holder]
+ex. http://localhost:8080/AutoSuggest/s?q=how%20t
 
 
 ============demo has been deployed over aws=====
 http://ec2-50-112-143-235.us-west-2.compute.amazonaws.com:8080/s?q=[placed holder]
 use above  link to see the demo.
+
+
+==========Unit Testing==========
+i have created CallService.java for unit testing. here i am creting thread pool and hitting webservice to get the response. i have created 100 thread, and 1000 task which are being used to hit the service.
